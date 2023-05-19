658beffe7aef505d5d1a48b034f9ba78be3e6c64
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-19 17:48:01 -0400

    change glfw window title

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "gl_textrenderer", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "gl_jump", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
