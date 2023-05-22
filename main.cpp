ce59f61562362e9ec1ce42e0ba081c29c034b418
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-22 13:19:07 -0400

    add shader class

--- main.cpp
+++ main.cpp
@@ -8,37 +8,12 @@
 
 #include "gl_textrenderer/gl_textrenderer.h"
 #include "gl_gridlines/gl_gridlines.h"
+#include "Shader.h"
 
 using namespace gl;
 
 const unsigned int SCREEN_WIDTH = 500;
 const unsigned int SCREEN_HEIGHT = 500;
-
-const std::string vertex_shader_source = R"(
-        #version 330 core
-        layout (location = 0) in vec2 aPos;
-
-        uniform mat4 projection;
-
-        void main()
-        {
-            gl_Position = projection * vec4(aPos.xy, 1, 1);
-        }
-)";
-
-const std::string fragment_shader_source = R"(
-        #version 330 core
-        out vec4 FragColor;
-
-        uniform vec3 color;
-
-        void main()
-        {
-            FragColor = vec4(color.xyz, 1.0f);
-        }
-)";
-
-unsigned int create_shader_program(const std::string& vertex_source, const std::string& fragment_source);
 
 void draw_rectangle(int rectangle_width, int rectangle_height, int rectangle_pos_x, int rectangle_pos_y);
 
@@ -74,7 +49,8 @@ int main()
 
     glbinding::initialize(glfwGetProcAddress);
 
-    unsigned int shaderProgram = create_shader_program(vertex_shader_source, fragment_shader_source);
+    Shader shader;
+    unsigned int shaderProgram = shader.get_shader_program();
     glUseProgram(shaderProgram);
     glm::mat4 projection = glm::ortho(0.0f, (float) SCREEN_WIDTH, 0.0f, (float) SCREEN_HEIGHT);
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
@@ -246,52 +222,6 @@ int main()
     }
 
     return 0;
-}
-
-unsigned int create_shader_program(const std::string& vertex_source, const std::string& fragment_source)
-{
-    // vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    const char* c_str_vertex = vertex_source.c_str();
-    glShaderSource(vertexShader, 1, &c_str_vertex, nullptr);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    const char* c_str_fragment = fragment_source.c_str();
-    glShaderSource(fragmentShader, 1, &c_str_fragment, nullptr);
-    glCompileShader(fragmentShader);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // link shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    return shaderProgram;
 }
 
 void draw_rectangle(int rectangle_width, int rectangle_height, int rectangle_pos_x, int rectangle_pos_y)
