7a5aad4560f2bc02a947cf2a5a9122213e97e822
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-27 15:36:55 -0400

    reformat Triangle.h

--- include/Triangle/Triangle.h
+++ include/Triangle/Triangle.h
@@ -2,15 +2,23 @@
 
 #include <iostream>
 #include <glbinding/gl/gl.h>
+
 using namespace gl;
 
 class Triangle
 {
 public:
-    Triangle(int triangle_width, int triangle_height, int triangle_pos_x, int triangle_pos_y);
+    Triangle(int triangle_width, int triangle_height, int triangle_pos_x,
+             int triangle_pos_y);
+
     ~Triangle();
+
     void draw(unsigned int shader_program, float r, float g, float b);
-    void update_position(int score, double delta_time, unsigned int screen_width, int reset_pos);
+
+    void
+    update_position(int score, double delta_time, unsigned int screen_width,
+                    int reset_pos);
+
     int triangle_width = 0;
     int triangle_height = 0;
     int triangle_pos_x = 0;
